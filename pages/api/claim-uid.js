7b5cd740719c1f6db4ecdcca6f4fddbdd7525a2c
Oscar Mullikin <oscarmullikin@Mac.lan>
2025-10-06 02:45:29 -0500

    jfois

--- pages/api/claim-uid.js
+++ pages/api/claim-uid.js
@@ -4,8 +4,13 @@ import { env } from '../../lib/env';
 import { logEvent } from '../../utils/logger';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  console.log('[claim-uid] Incoming request method:', req.method);
+
   if (req.method === 'OPTIONS') {
-    res.setHeader('Allow', 'POST, OPTIONS');
     return res.status(200).end();
   }
 
