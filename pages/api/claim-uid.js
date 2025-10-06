193cd861d2be0952e10aa443fad42dc21826d83c
Oscar Mullikin <oscarmullikin@Mac.lan>
2025-10-06 02:36:27 -0500

    dsifjd

--- pages/api/claim-uid.js
+++ pages/api/claim-uid.js
@@ -4,7 +4,13 @@ import { env } from '../../lib/env';
 import { logEvent } from '../../utils/logger';
 
 export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Allow', 'POST, OPTIONS');
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
+    res.setHeader('Allow', 'POST, OPTIONS');
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
