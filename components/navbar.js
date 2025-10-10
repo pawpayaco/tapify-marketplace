b617fbf459672406bb334aa80c19e48d5ac8dd59
Oscar Mullikin <oscarmullikin@Mac.lan>
2025-10-10 02:03:52 -0500

    Sync latest UID and Priority Display fixes

--- components/navbar.js
+++ components/navbar.js
@@ -21,7 +21,7 @@ export default function Navbar() {
         }}
       />
       <nav className="fixed top-0 left-0 right-0 z-50 border-b border-gray-200 shadow-sm bg-white/95 backdrop-blur-sm">
-        <div className="max-w-[1800px] mx-auto px-4 sm:px-8 lg:px-12 pb-2">
+        <div className="max-w-[1800px] mx-auto px-4 sm:px-8 lg:px-12 pt-2 pb-2">
         <div className="flex justify-between items-center h-16 sm:h-20">
           {/* Left: See Products Button */}
           <div className="flex-1 flex justify-start">
